a9e4af4f4ef8f3f718a1643b10691586907fe705
kauawinycyus-dev <kauawinycyus4@gmail.com>
2026-07-02 16:22:40 -0300

    fix: corrige caminho do css no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './app.css' // <-- CERTIFIQUE-SE DE QUE ESTA LINHA EXISTE E APONTA PARA O SEU ARQUIVO COM O @import "tailwindcss"
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
